15f94ddf64f1e57e4b52e2684fab5853664e0494
febryanmz <febryan.zainal@gmail.com>
2023-04-13 20:54:59 +0700

    hackerrank

--- Sales-by-Match.go
+++ Sales-by-Match.go
@@ -1,2 +1,21 @@
 package main
 
+import "fmt"
+
+func sockMerchant(n int32, ar []int32) int32 {
+    // Write your code here
+	var sockMap = make(map[int32]int32, 0)
+	var pair int32 = 0
+	for _, v := range ar {
+		sockMap[v]++
+		if v := sockMap[v]; v % 2 == 0 {
+			pair++
+		}
+	}
+	return pair
+
+}
+
+func main()  {
+	fmt.Println(sockMerchant(8, []int32{1,1,2,3,2,1,2,2,4}))
+}
